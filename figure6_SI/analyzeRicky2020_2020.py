3435d3a5d5e619e4828b95467ef30b60d939c5d3
Nencini Ricky <nenciric@dx8-919-10297.biocenter.helsinki.fi>
2022-04-28 10:05:44 +0300

    add figures

---
+++ figure6_SI/analyzeRicky2020_2020.py
@@ -0,0 +1,49 @@
+import sys
+import numpy as np
+import MDAnalysis as mda
+import matplotlib.pyplot as plt
+import os
+import fnmatch
+import yaml
+import gc
+import math
+import warnings
+import re
+import time
+
+from optparse import OptionParser
+from collections import OrderedDict
+from operator import add
+from linecache import getline
+
+
+sys.path.insert(1, '/home/nenciric/Documents/git/charged_molecules_binding/simulations_list/')
+import AnalysisToolbox as AT
+
+from datetime import date
+today = date.today()
+
+gc.collect()
+
+folder_path="//DATA/hector/200POPC_PN_NAs150mM/"
+systems=["sodium"]
+
+
+for file in os.listdir(folder_path):
+    input_corr_file = folder_path+os.fsdecode(file)
+    for system in systems:
+        if fnmatch.fnmatch(os.fsdecode(file), "*"+system+"*"):
+            newcomer=AT.AnalysisToolbox(folder_path,os.fsdecode(file),system,["ORDER_PARAMETER","BOX_DIMENSIONS"])
+            newcomer.add_new_folders()
+            newcomer.analysis_module()
+
+#folder_path="/media/nenciric/Ricky20201/simulations/"
+#systems=["etidocaine","TPP","SMS","dibucaine"]
+
+
+#for file in os.listdir(folder_path):
+#    input_corr_file = folder_path+os.fsdecode(file)
+#    for system in systems:
+#        if fnmatch.fnmatch(os.fsdecode(file), "*"+system+"*"):
+#            AT.AnalysisToolbox(folder_path,os.fsdecode(file),system,["ORDER_PARAMETER","BOX_DIMENSIONS"])
+                                        
